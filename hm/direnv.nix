55cc0f7def7fdb84c4847f28a9128bf275dc9db7
ash <x-hv2j5m@keysmash.solutions>
2025-05-22 17:49:24 +0100

    hm: Put direnv caches in ~/.cache/direnv

--- hm/direnv.nix
+++ hm/direnv.nix
@@ -14,6 +14,20 @@
       config = {
         global.hide_env_diff = true;
       };
+      stdlib = ''
+        # https://github.com/direnv/direnv/wiki/Customizing-cache-location#human-readable-directories
+        # NB: we use sha256sum rather than sha1sum
+        : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
+        declare -A direnv_layout_dirs
+        direnv_layout_dir() {
+            local hash path
+            echo "''${direnv_layout_dirs[$PWD]:=$(
+                hash="$(sha256sum - <<< "$PWD" | head -c40)"
+                path="''${PWD//[^a-zA-Z0-9]/-}"
+                echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+            )}"
+        }
+      '';
     };
   };
 }
