7289002899363b14034e18166a44b9a037746014
ash <x-hv2j5m@keysmash.solutions>
2025-05-26 13:42:38 +0100

    hm: Prevent deprecation warning from Keychain about `--agents`

--- hm/keychain.nix
+++ hm/keychain.nix
@@ -11,7 +11,6 @@
   config = lib.mkIf config.sys.keychain.enable {
     programs.keychain = {
       enable = true;
-      agents = [ "ssh" ];
       keys = [ "id_ed25519" ];
       extraFlags = [ "--noask" "--quiet" ];
     };
