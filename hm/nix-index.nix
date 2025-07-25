4aae2db76bf3e302935ebbc1b2dacf25f644f810
ash <x-hv2j5m@keysmash.solutions>
2025-07-25 17:44:56 +0100

    hm: Update nix-index usage for upstream change

--- hm/nix-index.nix
+++ hm/nix-index.nix
@@ -3,6 +3,6 @@
 if inputs ? nix-index-database
 then {
   programs.command-not-found.enable = false;
-  imports = [ inputs.nix-index-database.hmModules.nix-index ];
+  imports = [ inputs.nix-index-database.homeModules.nix-index ];
 }
 else {}
